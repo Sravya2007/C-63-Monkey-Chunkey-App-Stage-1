0b80f0f7879b532f069764b825428768f05e8277
Sravya2007 <sravyamarepally@outlook.com>
2020-11-06 18:34:26 +0530

    Stage 1

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Header } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+      text: '',
+      displayText: ''
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+       <Header backgroundColor = {"grey"} centerComponent = {{text: 'Monkey Chunkey', style: {color: 'black', fontSize: 20}}}/>
+       <TextInput onChangeText = {(text) => {
+         this.setState ({
+           text: text
+         })
+       }} value = {this.state.text} style = {styles.inputBox}/>
+
+      <TouchableOpacity style = {styles.goButton} onPress = {() => {
+        this.setState({
+          displayText: this.state.text
+        })
+      }}>
+        <Text style = {styles.buttonText}>Go</Text>
+      </TouchableOpacity>
+       <Text style = {styles.displayText}>
+        {this.state.displayText}
+       </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#b8b8b8',
   },
+
+  inputBox: {
+    marginTop: 260,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    borderStyle: 'dotted'
+  },
+
+  displayText: {
+    textAlign: 'center',
+    fontSize: 38
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30
+  }
 });
